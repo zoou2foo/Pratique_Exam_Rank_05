77f428a49809ae026c8150ebeed8ad5e7fc554d8
zoou2foo <zoou2foo@gmail.com>
2023-09-02 11:54:53 -0400

    some comment

--- cpp_module_00/Warlock.hpp
+++ cpp_module_00/Warlock.hpp
@@ -12,15 +12,15 @@ class Warlock
         std::string _title;
         Warlock(); //default
         Warlock(const Warlock &copy); //by copy
+        Warlock& operator=(const Warlock &rhs);
 
     public:
-        Warlock(std::string name, std::string title);
+        Warlock(std::string name, std::string title); //do I need to add const + &??
         ~Warlock(); //destructor
 
-        Warlock& operator=(const Warlock &rhs);
-        std::string getName(void) const;
+        std::string getName(void) const; //add const with string?? for both
         std::string getTitle(void) const;
-        void    setTitle(std::string title);
+        void    setTitle(std::string title); //const + &???
         void    introduce(void) const;
 
 };
